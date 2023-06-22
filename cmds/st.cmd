c9661145a66b23356d656a5bb309e2d4f0865841
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-06-22 16:53:16 +0200

    Updated docs. st.cmd for RS485 TO ETH gateway connection (Modbus TCP).

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -4,5 +4,9 @@ require siargo
 
 epicsEnvSet ("IOCNAME", "ioc22-siargo")
 
-iocshLoad ("$(siargo_DIR)siargo-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,SERIAL_PORT=/dev/ttyUSB0")
+# For flow meters connected via RS485
+#iocshLoad ("$(siargo_DIR)siargo-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,SERIAL_PORT=/dev/ttyUSB0")
+# For flowmeters connected via Ethernet (Modbus TCP)
+iocshLoad ("$(siargo_DIR)siargo-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.38")
+
 iocshLoad ("$(siargo_DIR)siargo-meter.iocsh", "ASYN_PORT_NAME=PORT1,MODBUS_ADR=1,PREFIX=Env-Gas:,DEV_NAME=MFM-FREIA")
